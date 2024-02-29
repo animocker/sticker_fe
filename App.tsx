87b341525896e6e258c7fb87ae70fbd95caa37f8
Irina Frolova <irina.frolova@admiralmarkets.com>
2024-02-29 14:40:59 +0700

    fix merge

--- App.tsx
+++ App.tsx
@@ -1,51 +1,66 @@
-import {Button, NativeModules, StyleSheet, Text, View} from 'react-native';
-import * as FileSystem from 'expo-file-system'
-import {Asset} from "expo-asset"
+import {StyleSheet, View} from "react-native";
+import * as FileSystem from "expo-file-system";
+import {Asset} from "expo-asset";
 import React, {useEffect} from "react";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import AvatarService from "./service/AvatarService";
 import {ElementType} from "./db/enum";
-import {tables} from "./db/elements";
 
-const Stack = createNativeStackNavigator();
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import AuthStartScreen from "./screens/auth/AuthStartScreen";
+import AuthLegalScreen from "./screens/auth/AuthLegalScreen";
+import AuthLoginScreen from "./screens/auth/AuthLoginScreen";
+import OnboardingStartScreen from "./screens/onboarding/OnboardingStartScreen";
+import OnboardingSelfieScreen from "./screens/onboarding/OnboardingSelfieScreen";
+import OnboardingManualCreateCharacterScreen from "./screens/onboarding/OnboardingManualCreateCharacterScreen";
 
+const Stack = createStackNavigator();
 
 export const loadDatabase = async () => {
-    const dbName = "lottie2.db"
-    const dbAsset = require("./assets/" + dbName)
-    const dbUri = Asset.fromModule(dbAsset).uri
-    const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`
+  const dbName = "lottie2.db";
+  const dbAsset = require("./assets/" + dbName);
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
 
-    const fileInfo = await FileSystem.getInfoAsync(dbFilePath)
-    if (!fileInfo.exists) {
-        await FileSystem.makeDirectoryAsync(
-            `${FileSystem.documentDirectory}SQLite`,
-            {intermediates: true}
-        );
-        await FileSystem.downloadAsync(dbUri, dbFilePath);
-    }
-}
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      {intermediates: true}
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+};
 
 export default function App() {
-    useEffect(() => {
-            loadDatabase().then(() => console.log("Database loaded")).catch((e) => console.error(e))
-        }
-    )
-    AvatarService.changeElement({elementType: ElementType.HAT, number: 2})
-    var lottie = AvatarService.getAnimation("HELLO")
-    return (
-        <View style={styles.container}>
-
-        </View>
-    );
+  useEffect(() => {
+    loadDatabase().then(() => console.log("Database loaded")).catch((e) => console.error(e));
+  }
+  );
+  AvatarService.changeElement({elementType: ElementType.HAT, number: 2});
+  var lottie = AvatarService.getAnimation("HELLO");
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="AuthStart"
+      >
+        <Stack.Screen name="AuthStart" component={AuthStartScreen} options={{ headerShown: false }}  />
+        <Stack.Screen name="AuthLegal" component={AuthLegalScreen} />
+        <Stack.Screen name="AuthLogin" component={AuthLoginScreen} />
+        <Stack.Screen name="OnboardingStart" component={OnboardingStartScreen} />
+        <Stack.Screen name="OnboardingSelfie" component={OnboardingSelfieScreen} />
+        <Stack.Screen name="OnboardingManualCreateCharacter" component={OnboardingManualCreateCharacterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
+  container: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
+    justifyContent: "center",
+  },
+});
