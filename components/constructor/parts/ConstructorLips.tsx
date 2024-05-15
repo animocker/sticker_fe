d645b46ee0b5a5e816bb327ddd117d956618606f
Irina Frolova <irina.frolova@admiralmarkets.com>
2024-05-15 11:42:53 +0700

    add icons

--- components/constructor/parts/ConstructorLips.tsx
+++ components/constructor/parts/ConstructorLips.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import {ScrollView, TouchableOpacity, StyleSheet} from "react-native";
+import {ScrollView, TouchableOpacity, StyleSheet, Image} from "react-native";
 import {elementsMenuStyles} from "../styles";
 import {ElementType} from "../../../backend/db/enum";
 import PropTypes from "prop-types";
 
-import LIPS_1 from "../icons/lips/lips_1.svg";
-import LIPS_2 from "../icons/lips/lips_2.svg";
-import LIPS_3 from "../icons/lips/lips_3.svg";
-import LIPS_4 from "../icons/lips/lips_4.svg";
-import LIPS_5 from "../icons/lips/lips_5.svg";
+const LIPS_1 = require("../icons/lips/lips_1.png");
+const LIPS_2 = require("../icons/lips/lips_2.png");
+const LIPS_3 = require("../icons/lips/lips_3.png");
+const LIPS_4 = require("../icons/lips/lips_4.png");
+const LIPS_5 = require("../icons/lips/lips_5.png");
 
 const LIPS_SETTINGS = [
-  {icon: <LIPS_1 />, name: "lips_1"},
-  {icon: <LIPS_2 />, name: "lips_2"},
-  {icon: <LIPS_3 />, name: "lips_3"},
-  {icon: <LIPS_4 />, name: "lips_4"},
-  {icon: <LIPS_5 />, name: "lips_5"},
+  {icon: <Image source={LIPS_1} />, name: "lips_1"},
+  {icon: <Image source={LIPS_2} />, name: "lips_2"},
+  {icon: <Image source={LIPS_3} />, name: "lips_3"},
+  {icon: <Image source={LIPS_4} />, name: "lips_4"},
+  {icon: <Image source={LIPS_5} />, name: "lips_5"},
 ];
 
 export const ConstructorLips = ({ changeElement }) => {
